7eb063cab33134e7cd78cd01f92fc0267e72339f
Aboup1010 <79264728+Aboup1010@users.noreply.github.com>
2025-12-26 22:28:47 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const button = document.getElementById("loveBtn");
+const message = document.getElementById("message");
+
+button.addEventListener("click", () => {
+    message.textContent = "💘 Xushkann, You are my heart 💘";
+    createHeart();
+});
+
+function createHeart() {
+    const heart = document.createElement("div");
+    heart.classList.add("heart");
+    heart.innerHTML = "❤️";
+
+    heart.style.left = Math.random() * 100 + "vw";
+    heart.style.fontSize = Math.random() * 20 + 20 + "px";
+    heart.style.animationDuration = Math.random() * 3 + 4 + "s";
+
+    document.body.appendChild(heart);
+
+    setTimeout(() => {
+        heart.remove();
+    }, 6000);
+}
